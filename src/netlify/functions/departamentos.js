35e733acd88da2290244426db8fec0df31d5a407
VictorCalbucura <111377379+VictorCalbucura@users.noreply.github.com>
2025-02-08 16:01:26 -0300

    fix

--- src/netlify/functions/departamentos.js
+++ src/netlify/functions/departamentos.js
@@ -12,6 +12,10 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log("Conectado a la base de datos");
+}).catch((error) => {
+  console.error("Error al conectar a la base de datos", error);
 });
 
 router.get("/", async (req, res) => {
